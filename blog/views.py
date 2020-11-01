ba452b647d96d1b5e22ecf7f9fc92eeadb8d5f56
tohfaakib <tnishan7@gmail.com>
2020-11-01 18:51:20 +0600

    form

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,36 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Post
+from .forms import EmailPostForm
 
 # Create your views here.
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    except EmptyPage:
-        posts = paginator.page(paginator.num_pages)
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#
+#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, day, month, year, post):
     post = get_object_or_404(Post, slug=post, publish__day=day, publish__month=month, publish__year=year)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+# def post_share(re)
